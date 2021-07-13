9b8afd0c690d345df5a58aa0d11ffd9a2a3e5250
emipa606 <emil.offermo@litet.net>
2021-07-13 22:10:56 +0200

    Mod updated for 1.3 and passed autotests

---
+++ Source/MassStorage/OnStartup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace StockpileAugmentations
+{
+    [StaticConstructorOnStartup]
+    public static class OnStartup
+    {
+        static OnStartup()
+        {
+            LongEventHandler.QueueLongEvent(Patcher, "Running patches", false, null);
+        }
+
+        private static void Patcher()
+        {
+            var harmony = new Harmony("net.spdskatr.factoryframework.patches");
+            harmony.Patch(typeof(ResourceCounter).GetMethod(nameof(ResourceCounter.UpdateResourceCounts)), null,
+                new HarmonyMethod(typeof(OnStartup), nameof(ResourceCounterPostfix)));
+        }
+
+        private static void ResourceCounterPostfix(ResourceCounter __instance)
+        {
+            var countedAmounts = Traverse.Create(__instance).Field("countedAmounts")
+                .GetValue<Dictionary<ThingDef, int>>();
+            var map = Traverse.Create(__instance).Field("map").GetValue<Map>();
+
+            try
+            {
+                map.listerBuildings.allBuildingsColonist.OfType<Building_MassStorageDevice>().ToList()
+                    .FindAll(b => b.internalStoredDef != null && b.ThingCount > 0)
+                    .ForEach(storage =>
+                    {
+                        if (storage.internalStoredDef.CountAsResource)
+                        {
+                            countedAmounts[storage.internalStoredDef] += storage.ThingCount;
+                        }
+                    });
+            }
+            catch (Exception ex)
+            {
+                Log.Error("SS Mass Storage caught exception while editing resource counts: " + ex);
+            }
+            finally
+            {
+                Traverse.Create(__instance).Field("countedAmounts").SetValue(countedAmounts);
+            }
+        }
+    }
+}
